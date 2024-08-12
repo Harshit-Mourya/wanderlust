56374fab294d254b382dd41ebdbd54984ff694e0
Harshit Mourya <mouryaharshit03@gmail.com>
2024-08-12 11:14:29 +0530

    created all routes

--- models/listing.js
+++ models/listing.js
@@ -6,7 +6,11 @@ const listingSchema = new Schema({
   description: String,
   image: {
     filename: String,
-    url: String,
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1440778303588-435521a205bc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
 
     // default:
     //   "https://images.unsplash.com/photo-1440778303588-435521a205bc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
