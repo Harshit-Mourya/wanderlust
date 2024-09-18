a6af8448af1a6d33ea3f99634dae6b1028b0cc85
Harshit Mourya <mouryaharshit03@gmail.com>
2024-09-19 00:38:55 +0530

    MVC Changes, star rating and router.route

--- routes/review.js
+++ routes/review.js
@@ -6,8 +6,8 @@ const {
   isLoggedIn,
   isReviewAuthor,
 } = require("../middleware.js");
-const Listing = require("../models/listing.js");
-const Review = require("../models/review.js");
+
+const reviewController = require("../controllers/reviews.js");
 
 //Reviews
 //Post Review Route
@@ -15,18 +15,7 @@ router.post(
   "/",
   isLoggedIn,
   validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    // console.log("saved");
-    res.redirect(`/listings/${listing._id}`);
-  })
+  wrapAsync(reviewController.createReview)
 );
 
 //Delete Review Route
@@ -34,14 +23,7 @@ router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review deleted!");
-    res.redirect(`/listings/${id}`);
-  })
+  wrapAsync(reviewController.destroyReview)
 );
 
 module.exports = router;
