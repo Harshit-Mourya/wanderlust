bf00cfbd6b9f41d15bfeedb6327c744acebffabb
Harshit Mourya <mouryaharshit03@gmail.com>
2024-09-20 16:39:49 +0530

    filters and searchbar frontend only

--- app.js
+++ app.js
@@ -60,10 +60,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/", (req, res) => {
-  res.send("Root");
-});
-
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
